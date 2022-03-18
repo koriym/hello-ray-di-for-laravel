cd825bff91a8630a32263be0f2d2aeeb2d5cb655
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-03-18 19:05:11 +0900

    Inject Provider and set PostConstruct

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Attribute\Loggable;
 use App\Domain\Double\DoubleInterface;
+use Ray\Di\Di\Inject;
+use Ray\Di\Di\PostConstruct;
+use Ray\Di\Di\Set;
+use Ray\Di\ProviderInterface;
 
 class HelloController extends Controller
 {
+    private ProviderInterface $doubleProvider;
+
     public function __construct(
+        // Inject depedency
         private readonly DoubleInterface $double
     ){}
 
