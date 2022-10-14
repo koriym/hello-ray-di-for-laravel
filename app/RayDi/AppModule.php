09f38eab5a2b967058491c0e9249203747d96f6d
tsuchiya <tsuchiya@bengo4.com>
2022-10-15 00:56:53 +0900

    Copy from package
    cp -r vendor/ray/ray-di-for-laravel/RayDi app
    cp -r vendor/ray/ray-di-for-laravel/storage/ storage

---
+++ app/RayDi/AppModule.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\RayDi;
+
+use Ray\Di\AbstractModule;
+
+final class AppModule extends AbstractModule
+{
+    protected function configure(): void
+    {
+    }
+}
